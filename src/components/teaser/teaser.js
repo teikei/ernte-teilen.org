4734bbbecf444057d82c69a9d7472bac67ed8451
Simon Jockers <hello@simonjockers.de>
2018-07-31 12:21:43 +0200

    Add propTypes to Teaser component

--- src/components/teaser/teaser.js
+++ src/components/teaser/teaser.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './teaser.scss'
 
-export default ({ title }) => (
+const Teaser = ({ title }) => (
   <a href="/" className="teaser">
     <h3 className="teaser__title">
       {title}
     </h3>
   </a>
 )
+
+Teaser.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Teaser
