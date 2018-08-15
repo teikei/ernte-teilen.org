185b5468c6f0c0126a9a94582afb9d5202a1f0d3
Simon Jockers <hello@simonjockers.de>
2018-08-15 16:16:50 +0200

    Add logo to footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,4 +1,14 @@
 import React from 'react'
 import './styles.scss'
 
-export default () => <footer href="/" className="et--footer" />
+export default () => (
+  <footer href="/" className="et--footer">
+    <div className="bx--grid">
+      <div className="bx--row">
+        <a className="et--footer__logo" href="/">
+          {'Ernte teilen!'}
+        </a>
+      </div>
+    </div>
+  </footer>
+)
