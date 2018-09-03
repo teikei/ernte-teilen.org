5568ab886225c1c576bcdc4dfcb29ad62bcc6296
Simon Jockers <hello@simonjockers.de>
2018-09-03 18:28:13 +0200

    Teaser: Optimize styling for small screens

--- src/components/Teaser/index.js
+++ src/components/Teaser/index.js
@@ -2,11 +2,8 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './styles.scss'
 
-import logo from '../../../static/img/teaser_membership.png'
-
 const Teaser = ({ title, text }) => (
   <a href="/" className="et--teaser">
-    <img className="et--teaser__image" src={logo} alt="Logo" />
     <h3 className="et--teaser__title">
       {title}
     </h3>
