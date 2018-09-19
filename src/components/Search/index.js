575b6d1638284b410ae3e5a1c7ad173fd5a3b389
Simon Jockers <hello@simonjockers.de>
2018-09-19 15:43:02 +0200

    Search: Add label to search input

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -7,13 +7,17 @@ import './styles.scss'
 const Search = ({ t }) => (
   <div href="/" className="et--search">
     <div className="et--search__form">
-      <div id="teikei-search" data-base-url="/karte/#" />
+      {/* eslint-disable-next-line jsx-a11y/label-has-for */}
+      <label className="et--search__label">
+        {t.search.label}
+        <div className="et--search__input" id="teikei-search" data-base-url="/karte/#" />
+      </label>
     </div>
   </div>
 )
 
 Search.propTypes = {
-  t: PropTypes.shape().isRequired
+  t: PropTypes.shape().isRequired,
 }
 
 export default Search
