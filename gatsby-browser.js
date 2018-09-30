a9a14a0d830259f4edb51972d5fce4834aa2f1e4
Christian Rijke <info@christianrijke.de>
2018-09-30 16:48:26 +0200

    Make sure the teikei bundle is injected only once (and not repeatedly on location changes).

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,11 @@
 exports.onRouteUpdate = () => {
   const teikeiEmbed = document.getElementById('teikei-app') || document.getElementById('teikei-search')
+  const teikeiScript = document.getElementById('teikei-bundle')
 
-  if (teikeiEmbed) {
+  if (teikeiEmbed && !teikeiScript) {
     const script = document.createElement('script')
     script.src = 'https://map-staging.teikei.allmende.io/main.js'
+    script.id = 'teikei-bundle'
     script.async = true
     teikeiEmbed.insertAdjacentElement('afterend', script)
   }
