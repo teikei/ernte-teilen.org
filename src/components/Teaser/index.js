f3aa620be26ad288c7f1ce4fad7ac267f38aea7d
Simon Jockers <hello@simonjockers.de>
2018-10-02 11:32:10 +0200

    Home: Add links to teasers

--- src/components/Teaser/index.js
+++ src/components/Teaser/index.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './styles.scss'
+import { Link } from 'gatsby'
 
-const Teaser = ({ title, text, slug }) => (
-  <a href="/" className={`et--teaser et--teaser--${slug}`}>
+const Teaser = ({
+  title, text, slug, href,
+}) => (
+  <Link to={href} className={`et--teaser et--teaser--${slug}`}>
     <h3 className="et--teaser__title">
       {title}
     </h3>
     <p className="et--teaser__text">
       {text}
     </p>
-  </a>
+  </Link>
 )
 
 Teaser.propTypes = {
   slug: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
+  href: PropTypes.string.isRequired,
 }
 
 export default Teaser
--- src/components/TeaserGroup/index.js
+++ src/components/TeaserGroup/index.js
@@ -8,9 +8,11 @@ import './styles.scss'
 const TeaserGroup = ({ teasers }) => (
   <div className="et--teaser-group bx--grid">
     <div className="bx--row">
-      {teasers.map(({ title, text, slug }) => (
+      {teasers.map(({
+        title, text, slug, href,
+      }) => (
         <div className="bx--col-sm-4" key={title}>
-          <Teaser title={title} text={text} slug={slug} />
+          <Teaser title={title} text={text} slug={slug} href={href} />
         </div>
       ))}
     </div>
@@ -20,8 +22,10 @@ const TeaserGroup = ({ teasers }) => (
 TeaserGroup.propTypes = {
   teasers: PropTypes.arrayOf(
     PropTypes.shape({
+      slug: PropTypes.string.isRequired,
       title: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
     }),
   ).isRequired,
 }
@@ -35,6 +39,7 @@ export const query = graphql`
         slug
         title
         text
+        href
       }
     }
   }
