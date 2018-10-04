2482f3ad26296ab8be576ee2a9215d4f65248bf8
Simon Jockers <hello@simonjockers.de>
2018-10-04 19:04:59 +0200

    Testimonials: Fix issues component width

--- src/components/Testimonial/index.js
+++ src/components/Testimonial/index.js
@@ -7,33 +7,35 @@ import './styles.scss'
 const Testimonial = ({
   title, text, name, description, quote, image,
 }) => (
-  <article className="et--testimonial bx--grid">
-    <div className="bx--row">
-      <div className="bx--col-sm-10 bx--col-md-5">
-        <blockquote className="et--testimonial__quote">
-          {quote}
-        </blockquote>
-      </div>
-      <div className="bx--col-md-3">
-        <figure className="et--testimonial__figure">
-          <Img className="et--testimonial__figure__image" sizes={image.sizes} alt="" />
-          <figcaption>
-            <p className="et--testimonial__name">
-              {name}
-            </p>
-            <p className="et--testimonial__description">
-              {description}
-            </p>
-          </figcaption>
-        </figure>
-      </div>
-      <div className="bx--col-md-4">
-        <h3 className="et--testimonial__title">
-          {title}
-        </h3>
-        <p className="et--testimonial__text">
-          {text}
-        </p>
+  <article className="et--testimonial">
+    <div className="bx--grid">
+      <div className="bx--row">
+        <div className="bx--col-sm-10 bx--col-md-5">
+          <blockquote className="et--testimonial__quote">
+            {quote}
+          </blockquote>
+        </div>
+        <div className="bx--col-md-3">
+          <figure className="et--testimonial__figure">
+            <Img className="et--testimonial__figure__image" sizes={image.sizes} alt="" />
+            <figcaption>
+              <p className="et--testimonial__name">
+                {name}
+              </p>
+              <p className="et--testimonial__description">
+                {description}
+              </p>
+            </figcaption>
+          </figure>
+        </div>
+        <div className="bx--col-md-4">
+          <h3 className="et--testimonial__title">
+            {title}
+          </h3>
+          <p className="et--testimonial__text">
+            {text}
+          </p>
+        </div>
       </div>
     </div>
   </article>
