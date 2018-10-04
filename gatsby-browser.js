ce0ce0a2d03029e2218d0979606ec060c8433e7f
Simon Jockers <hello@simonjockers.de>
2018-10-04 19:36:10 +0200

    Explicitly add `rel` and `type` attributes for teikei CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,8 +5,10 @@ exports.onRouteUpdate = () => {
   if (teikeiEmbed && !teikeiScript) {
     const styles = document.createElement('link')
     const script = document.createElement('script')
-    styles.href = `${process.env.GATSBY_TEIKEI_BUNDLES_URL}/main.css`
-    script.src = `${process.env.GATSBY_TEIKEI_BUNDLES_URL}/main.js`
+    styles.setAttribute('rel', 'stylesheet')
+    styles.setAttribute('type', 'text/css')
+    styles.setAttribute('href', `${process.env.GATSBY_TEIKEI_BUNDLES_URL}/main.css`)
+    script.setAttribute('src', `${process.env.GATSBY_TEIKEI_BUNDLES_URL}/main.js`)
     script.id = 'teikei-bundle'
     script.async = true
     teikeiEmbed.insertAdjacentElement('afterend', script)
